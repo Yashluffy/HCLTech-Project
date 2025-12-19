575bf3bed58546a26ba5c52bb9ba6f5d8d1235aa
Yash Mahendia <mahendiayashu@gmail.com>
2025-12-19 19:19:42 +0530

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 # =====================================================
 # STREAMLIT CONFIG
